d22a16d98ed992ee84068296fdb7359c0593e884
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-01-23 16:39:52 -0500

    setup tailwind, added photos, started hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
-
+import { motion } from "framer-motion";
+import '../styles/hero-styles.css';
 
 const Hero = () => {
-    return (
-      <div>
-      <p> hiii </p>
-      </div>
-    );
-  };
-  
-  export default Hero;
+  return (
+    <section id="Hero" className="container">
+      <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+        <div className="hero-header"> Hey, I&apos;m Alex. </div>
+        <div className="hero-subheader"> I code and stuff. </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
