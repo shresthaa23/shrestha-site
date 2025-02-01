91845e254384329bb438f9138add02d110fb393a
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-01-31 20:48:28 -0500

    hero updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import '../styles/hero-styles.css';
 const Hero = () => {
   return (
     <section id="Hero" className="container">
-      <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+      <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="hero-content">
         <div className="hero-header"> Hey, I&apos;m Alex. </div>
-        <p className="hero-subheader"> I code and stuff. This is a work in progress!</p>
+        <p className="hero-subheader"> I code and stuff. This is a work in progress! </p>
       </motion.div>
     </section>
   );
