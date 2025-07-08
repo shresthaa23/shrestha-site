bd45e492e578e153ff473100ae1a161c9f752289
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-07-07 23:25:21 -0400

    hero page lol

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section id="Hero" className={styles.container}>
       <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className={styles.heroContent}>
         <div className={styles.heroHeader}> Hey, I&apos;m Alex. </div>
-        <p className={styles.heroSubheader}> I code and do other things. </p>
+        <p className={styles.heroSubheader}> I code and do other things <br/>(including updating this soon) </p>
       </motion.div>
     </section>
   );
