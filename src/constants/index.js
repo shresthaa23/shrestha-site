d249584f212503127c9c883373d947abebf88d63
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-09-03 18:31:10 -0400

    added new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -41,11 +41,14 @@ export const projects = [
       path: "projects/Honeypot.png", title: "Honeypot Project", description: "Engineered Linux containers simulating academic institutions to study attack behavior under distinct welcome banner messages, analyzing over 9000 attacks to uncover patterns in attacker behavior.", languages: "Bash Shell Scripting, Python, Git", link: "", 
     },
     { 
-      path: "projects/TouchGrass.png", title: "Touching Grass App", description: "Developed the foundational components of a social media app in 36 hours during the Bitcamp Hackathon. While the app was in its early stages, it allowed users to take photos of environmental destruction, earn points and track progress, inspired by Instagram and Pokemon Go.", languages: "React Native, JavaScript, CSS, Git", link: "", 
+      path: "projects/BrowserBuddy.png", title: "Browser Buddy Extension", description: "Created a virtual wellness pet extension in under 24 hours at Bitcamp Hackathon. The extension implements a pomodoro timer to help user take active breaks and gamify working sessions with XP and shop tokens for the customizable pet.", languages: "React, Typescript, Git, CSS", link: "", 
     },
     { 
       path: "projects/Maze.png", title: "Maze Solver", description: "Converted large-scale mazes into graphs, mapping all nodes to an x-y coordinate system. Found the optimal solution 100% of the time using Dijkstra's algorithm, breadth-first and depth-first searches.", languages: "Java", link: "", 
-    },      
+    },  
+    { 
+      path: "projects/TouchGrass.png", title: "Touching Grass App", description: "Developed the foundational components of a social media app in 36 hours during the Bitcamp Hackathon. While the app was in its early stages, it allowed users to take photos of environmental destruction, earn points and track progress, inspired by Instagram and Pokemon Go.", languages: "React Native, JavaScript, CSS, Git", link: "", 
+    },
     { 
       path: "projects/Soccer.png", title: "Dynamic Soccer Ball", description: "Prototyped an app for a dynamic soccer ball that tracks training sessions and statistics, allowing users to compare with friends. Went through the product management lifecycle, from conceptualizing to MVP creation, financial planning and prototyping.", languages: "Figma", link: "https://www.figma.com/proto/riuULK8sholAHFUfmXm2FR/Dynamic-Soccer-Ball-App?node-id=2-2&p=f&t=9Dyg8KCzjmHoqDVz-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=2%3A2",
     },
