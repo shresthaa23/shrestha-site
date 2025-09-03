6cd97a2a47b88750c65d88ea5d099312b11d90bb
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-09-03 10:33:43 -0400

    slight about update more to come

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,18 @@
 import styles from "../styles/about.module.css";
 // import Marquee from "react-fast-marquee";
 // import { aboutMe } from "../constants";
-import { useState } from "react";
+// import { useState } from "react";
 
 const About = () => {
-  const [currentImage, setCurrentImage] = useState(`Me.jpg`);
+  // const [currentImage, setCurrentImage] = useState(`Me.jpg`);
 
-  const handleImageClick = () => {
-    setCurrentImage((prevImage) =>
-      prevImage === `Me2.jpg`
-        ? `Me.jpg`
-        : `Me2.jpg`
-    );
-  };
+  // const handleImageClick = () => {
+  //   setCurrentImage((prevImage) =>
+  //     prevImage === `Me2.jpg`
+  //       ? `Me.jpg`
+  //       : `Me2.jpg`
+  //   );
+  // };
   
   return (
     <section id="About" className={styles.container}>
@@ -22,12 +22,8 @@ const About = () => {
           <h1 className={styles.myName}>Alex Shrestha</h1>
           <div className={styles.aboutMe}>
             <p>
-              I&apos;m an undergraduate student at{" "}
-              <span className={styles.bolded}>The University of Maryland, College Park</span>, studying{" "}
-              <span className={styles.bolded}>Computer Science</span> with a minor in Economics. I&apos;m
-              also part of the <span className={styles.bolded}>Cybersecurity (ACES) Honors College</span>.
-              I&apos;m driven with a passion to learn, develop meaningful solutions and push the boundaries of what&apos;s possible by combining logical coding and innovative problem-solving. Check
-              out my{" "}
+              I&apos;m a junior <span className={styles.bolded}>Computer Science</span> student at{" "}
+              <span className={styles.bolded}>The University of Maryland, College Park</span>, with a minor in Economics. I&apos;m passionate about <span className={styles.bolded}>software development</span> with direct experience in full-stack development, low-level projects, and research. I&apos;ve also recently been interested in <span className={styles.bolded}>quantum computing</span> and its applications in real-world scenarios. I&apos;m driven to continously learn, develop impactful solutions and push the boundaries of technology. Check out my{" "}
               <a
                 className={styles.resume}
                 target="_blank"
@@ -50,10 +46,21 @@ const About = () => {
             <p>
               🦇👨 | 🏈 | 🐦🟣 | 🏀👑 | 🎬 | 💪🏋️‍♂️ | 🎮🕹️ | 🥩 | 🍣 | 💻 | 🛏️💤
             </p>
+            {/* <h3 className={styles.bolded}>Relevant Classes:</h3>
+            <ul>
+              <li>👨‍💻 Advanced Data Structures</li>
+              <li>👨‍💻 Algorithms</li>
+              <li>👨‍💻 Operating Systems</li>
+              <li>👨‍💻 Quantum Computing</li>
+              <li>👨‍💻 Quantum Information Science</li>
+              <li>👨‍💻 Quantum Algorithms</li>
+              <li>👨‍💻 Quantum Communication</li>
+              <li>👨‍💻 Quantum Cryptography</li>
+            </ul> */}
           </div>
         </div>
-        <div className={styles.photo} onClick={handleImageClick}>
-          <img src={currentImage} alt="Photo of Me" className={styles.profilePhoto} />
+        <div className={styles.photo}>
+          <img src={"Me.jpg"} alt="Photo of Me" className={styles.profilePhoto} />
         </div>
         {/* <Marquee speed={50} pauseOnHover>
           {likes.map((img, index) => (
