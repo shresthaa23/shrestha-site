932b0c6d376d0e6f1f6c6a6a6085e36e84d08ef5
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-09-03 23:47:40 -0400

    stars tres

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,29 +4,9 @@ import styles from '../styles/hero.module.css';
 const Hero = () => {
   return (
     <section id="Hero" className={styles.container}>
+      <div className={styles.shootingStar3}></div>
       <div className={styles.shootingStar}></div>
-        <div className={styles.shootingStarsContainer}>
-        {Array.from({ length: 10 }).map((_, i) => {
-          // random top position (vh units so it scales with screen height)
-          const top = Math.random() * 100;
-          // random delay between 0–8s
-          const delay = Math.random() * 8;
-          // random duration (speed)
-          const duration = 6 + Math.random() * 4; // between 6–10s
-
-          return (
-            <div
-              key={i}
-              className="styles.shootingStar"
-              style={{
-                top: `${top}vh`,
-                animationDelay: `${delay}s`,
-                animationDuration: `${duration}s`
-              }}
-            />
-          );
-        })}
-      </div>
+      <div className={styles.shootingStar2}></div>
 
 
       <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className={styles.heroContent}>
